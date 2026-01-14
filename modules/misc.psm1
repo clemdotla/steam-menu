a01698f623a29428f9b90092e9ce491e54ac7f05
Clem <58120735+clemdotla@users.noreply.github.com>
2026-01-14 14:36:00 +0100

    Testing auto-imports

---
+++ modules/misc.psm1
@@ -0,0 +1,23 @@
+# Colors Black, DarkBlue, DarkGreen, DarkCyan, DarkRed, DarkMagenta, DarkYellow, Gray, DarkGray, Blue, Green, Cyan, Red, Magenta, Yellow, White
+function Log {
+    param ([string]$Type, [string]$Message, [boolean]$NoNewline = $false)
+    
+    $Type = $Type.ToUpper()
+    switch ($Type) {
+        "OK"   { $foreground = "Green" }
+        "INFO" { $foreground = "Cyan" }
+        "ERR"  { $foreground = "Red" }
+        "WARN" { $foreground = "Yellow" }
+        "LOG"  { $foreground = "Magenta" }
+        "AUX"  { $foreground = "DarkGray" }
+        default { $foreground = "White" }
+    }
+
+    $date = Get-Date -Format "HH:mm:ss"
+    $prefix = if ($NoNewline) { "`r[$date] " } else { "[$date] " }
+    Write-Host $prefix -ForegroundColor "Cyan" -NoNewline
+
+    Write-Host [$Type] $Message -ForegroundColor $foreground -NoNewline:$NoNewline
+}
+
+Export-ModuleMember -Function Log
